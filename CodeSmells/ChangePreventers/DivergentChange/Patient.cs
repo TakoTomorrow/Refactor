26a709228a2e49f057bd52900f9aed8d5d146aa2
Tako <k77uj8ts24@gmail.com>
2024-07-16 01:18:05 +0800

    Refactor/divergent change (#1)
    Refactor Patient.
    
    Co-authored-by: Tomorrow <k77uj8ts25@gmail.com>

---
+++ CodeSmells/ChangePreventers/DivergentChange/Patient.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeSmells.ChangePreventers.DivergentChange;
+
+public class Patient(string patientName, IEnumerable<string> treatmentHistory)
+{
+    public string PatientName { get; private set; } = patientName;
+
+    public List<string> TreatmentHistory { get; private set; } = treatmentHistory.ToList();
+
+    public void ExportTreatmentHistory(string fileName)
+    {
+        using var writer = new StreamWriter(fileName);
+        foreach (var treat in TreatmentHistory)
+        {
+            writer.WriteLine(json);
+        }
+    }
+}
